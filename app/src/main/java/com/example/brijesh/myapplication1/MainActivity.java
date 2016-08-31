c05f56aae1488719167421e54397a5626c73518e
Brijesh <sanubrijesh.prajapati@gmail.com>
2016-08-31 12:18:14 +0530

    this is my first commit

---
+++ app/src/main/java/com/example/brijesh/myapplication1/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.brijesh.myapplication1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this,"Brijesh",Toast.LENGTH_LONG).show();
+    }
+}
