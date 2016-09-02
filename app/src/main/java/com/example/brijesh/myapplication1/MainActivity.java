54102d4c8f1992d94478ec6625c3b00e0c38a499
Brijesh <sanubrijesh.prajapati@gmail.com>
2016-09-02 19:12:29 +0530

    improve brijesh to brijesh prajapati.

--- app/src/main/java/com/example/brijesh/myapplication1/MainActivity.java
+++ app/src/main/java/com/example/brijesh/myapplication1/MainActivity.java
@@ -7,19 +7,16 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity
 {
 
+    int ii;
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this,"Brijesh",Toast.LENGTH_LONG).show();
-        int res=fun();
-        Toast.makeText(this,res,Toast.LENGTH_LONG).show();
-    }
-    private int fun()
-    {
-        int i=5;
-        int ii=i+90;
-       return ii;
+        Toast.makeText(this,"Brijesh Prajapati",Toast.LENGTH_LONG).show();
+        //int res=fun();
+       // Toast.makeText(this,res,Toast.LENGTH_LONG).show();
     }
 }
